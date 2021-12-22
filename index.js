1027d5b882af22e745778e47ddddfb7c1473028d
Felix Snyder <database137.dev@hotmail.com>
2021-12-22 10:45:24 -0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function parallax() {
-    var parallax = document.getElementById("parallax");
-    parallax.style.top = -(window.pageYOffset / 4)+'px';
-  }
-  
-  window.addEventListener("scroll", parallax, false);
+  var parallax = document.getElementById("parallax");
+  parallax.style.top = -(window.pageYOffset / 4)+'px';
+}
+
+window.addEventListener("scroll", parallax, false);

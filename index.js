8acd0fd0b9dcc4cc6371609a15a548c9527bb14f
DataBase <dev.database137@outlook.com>
2022-01-09 17:43:23 -0800

    update

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 function parallax() {
-  var parallax = document.getElementById("parallax");
+  var parallax = document.querySelector(".parallax");
   parallax.style.top = -(window.pageYOffset / 4)+'px';
 }
 
 window.addEventListener("scroll", parallax, false);
-
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
-
-const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".links");
-
-navToggle.addEventListener("click", function () {
-  // console.log(links.classList);
-  // console.log(links.classList.contains("random"));
-  // console.log(links.classList.contains("links"));
-  // if (links.classList.contains("show-links")) {
-  //   links.classList.remove("show-links");
-  // } else {
-  //   links.classList.add("show-links");
-  // }
-  links.classList.toggle("show-links");
-});

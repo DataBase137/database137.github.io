98e1b69f48c9f9e62ac45b9af99be970e3fc0f41
DataBase <dev.database137@outlook.com>
2022-01-15 11:04:02 -0800

    update

--- index.js
+++
@@ -1,6 +0,0 @@
-/* function parallax() {
-  var parallax = document.querySelector(".parallax");
-  parallax.style.top = -(window.pageYOffset / 4)+'px';
-}
-
-window.addEventListener("scroll", parallax, false); */
